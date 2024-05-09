cc3d711858b76f3aef52179009373520799d7062
JorgeDev <jorgehumbertocolindres@gmail.com>
2024-05-09 12:34:00 -0600

    Rendering the 'form' if we are in the index page.
    Form Validation (todo)
    
    Consult Recipes with 'searchRecipes' from form values
    
    Adding Categories for the SELECT

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,48 @@
-import { NavLink } from "react-router-dom";
+import { useMemo, useEffect, useState, ChangeEvent, FormEvent } from "react";
+import { NavLink, useLocation } from "react-router-dom";
+
+import { useAppStore } from "../stores/useAppStore";
 
 export default function Header() {
+
+    const [searchFilters, setSearchFilters] = useState({
+        ingredient: '',
+        category: ''
+    });
+
+    const { pathname } = useLocation();
+    const isHome = useMemo(() => pathname === '/', [pathname]);
+
+    const fetchCategories = useAppStore((state) => state.fetchCategories);
+    const categories = useAppStore((state) => state.categories);
+    const searchRecipes = useAppStore((state) => state.searchRecipes);
+
+    useEffect(() => {
+        fetchCategories();
+    }, []);
+
+    const handleChange = (e: ChangeEvent<HTMLInputElement> | ChangeEvent<HTMLSelectElement>) => {
+        setSearchFilters({
+            ...searchFilters,
+            [e.target.name]: e.target.value
+        });
+    };
+
+    const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+
+        // todo: Validar
+        if (Object.values(searchFilters).includes('')) {
+            console.log('T  odos los campos son obligatorios');
+            return;
+        }
+
+        // Consultar Recetas
+        searchRecipes(searchFilters);
+    };
+
     return (
-        <header className="bg-slate-800">
+        <header className={`${isHome ? 'bg-header bg-center bg-cover' : 'bg-slate-800'}`}>
             <div className="mx-auto container px-5 py-16">
                 <div className="flex justify-between items-center">
                     <div>
@@ -28,6 +68,59 @@ export default function Header() {
                         >Favoritos</NavLink>
                     </nav>
                 </div>
+
+                {isHome && (
+                    <form
+                        className="md:w-1/2 2xl:w-1/3 bg-orange-400 my-32 p-10 rounded-lg shadow space-y-6"
+                        onSubmit={handleSubmit}
+                    >
+                        <div className="space-y-4">
+                            <label
+                                htmlFor="ingredient"
+                                className="block text-white uppercase font-extrabold text-lg"
+                            >Nombre o Ingredientes</label>
+
+                            <input
+                                type="text"
+                                id="ingredient"
+                                name="ingredient"
+                                className="p-3 w-full rounded-lg focus:outline-none"
+                                placeholder="Nombre o Ingrediente. Ej. Vodka, Tequila, Coffee"
+                                onChange={handleChange}
+                                value={searchFilters.ingredient}
+                            />
+                        </div>
+
+                        <div className="space-y-4">
+                            <label
+                                htmlFor="categorie"
+                                className="block text-white uppercase font-extrabold text-lg"
+                            >Categoria</label>
+
+                            <select
+                                name="category"
+                                id="category"
+                                className="p-3 w-full rounded-lg focus:outline-none"
+                                onChange={handleChange}
+                                value={searchFilters.category}
+                            >
+                                <option value="">-- Seleccione --</option>
+                                {categories.drinks.map((category, index) => (
+                                    <option
+                                        key={index}
+                                        value={category.strCategory}
+                                    >{category.strCategory}</option>
+                                ))}
+                            </select>
+                        </div>
+
+                        <input
+                            type="submit"
+                            value="Buscar Recetas"
+                            className="cursor-pointer bg-orange-800 hover:bg-orange-900 transition-colors duration-300 text-white font-extrabold w-full p-2 rounded-lg uppercase"
+                        />
+                    </form>
+                )}
             </div>
         </header>
     );
