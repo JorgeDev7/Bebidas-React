5b7a312f951dca530ce5b5d859b1ec7e03b8a6f0
JorgeDev <jorgehumbertocolindres@gmail.com>
2024-05-09 12:35:53 -0600

    States for drinks, selectedRecipe and Modal
    functions 'searchRecipes', 'selectRecipe' and 'closeModal' created with his own Service function

--- src/stores/recipeSlide.ts
+++ src/stores/recipeSlide.ts
@@ -1,20 +1,51 @@
 import { StateCreator } from "zustand";
-import { getCategories } from "../services/RecipeService";
-import type { Categories } from "../types";
+import { getCategories, getRecipeById, getRecipes } from "../services/RecipeService";
+import type { Categories, Drink, Drinks, Recipe, SearchFilter } from "../types";
 
 export type RecipesSlideType = {
     categories: Categories,
-    fetchCategories: () => Promise<void>;
+    drinks: Drinks,
+    selectedRecipe: Recipe,
+    modal: boolean,
+    fetchCategories: () => Promise<void>,
+    searchRecipes: (searchFilters: SearchFilter) => Promise<void>,
+    selectRecipe: (id: Drink['idDrink']) => Promise<void>,
+    closeModal: () => void;
 };
 
 export const createRecipesSlice: StateCreator<RecipesSlideType> = (set) => ({
     categories: {
         drinks: []
     },
+    drinks: {
+        drinks: []
+    },
+    selectedRecipe: {} as Recipe,
+    modal: false,
     fetchCategories: async () => {
         const categories = await getCategories();
         set({
             categories
+        });
+    },
+    searchRecipes: async (filters) => {
+        const drinks = await getRecipes(filters);
+        set({
+            drinks
+        });
+    },
+    selectRecipe: async (id) => {
+        const selectedRecipe = await getRecipeById(id);
+
+        set({
+            selectedRecipe,
+            modal: true
+        });
+    },
+    closeModal: () => {
+        set({
+            modal: false,
+            selectedRecipe: {} as Recipe
         });
     }
 });
