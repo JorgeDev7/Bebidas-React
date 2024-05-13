79e3af8d52c8fa9199242ccde42565391014e8d2
JorgeDev <jorgehumbertocolindres@gmail.com>
2024-05-13 11:09:42 -0600

    adding Notification

--- src/layouts/Layout.tsx
+++ src/layouts/Layout.tsx
@@ -4,6 +4,7 @@ import Header from "../components/Header";
 import Modal from "../components/Modal";
 
 import { useAppStore } from "../stores/useAppStore";
+import Notification from "../components/Notification";
 
 export default function Layout() {
 
@@ -21,6 +22,7 @@ export default function Layout() {
             </main>
 
             <Modal />
+            <Notification />
         </>
     );
 }
