1300099518aad2dabc7966b233f5c18511d4d4f4
JorgeDev <jorgehumbertocolindres@gmail.com>
2024-05-13 11:24:49 -0600

    adding lazy load

--- src/router.tsx
+++ src/router.tsx
@@ -1,7 +1,8 @@
+import { lazy, Suspense } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import IndexPage from './views/IndexPage';
-import FavoritesPage from './views/FavoritesPage';
 import Layout from './layouts/Layout';
+const IndexPage = lazy(() => import('./views/IndexPage'));
+const FavoritesPage = lazy(() => import("./views/FavoritesPage"));
 
 export default function AppRouter() {
     return (
@@ -10,12 +11,20 @@ export default function AppRouter() {
                 <Route element={<Layout />}>
                     <Route
                         path='/'
-                        element={<IndexPage />}
+                        element={
+                            <Suspense fallback="Cargando...">
+                                <IndexPage />
+                            </Suspense>
+                        }
                         index
                     />
                     <Route
                         path='/favoritos'
-                        element={<FavoritesPage />}
+                        element={
+                            <Suspense fallback="Cargando...">
+                                <FavoritesPage />
+                            </Suspense>
+                        }
                     />
                 </Route>
             </Routes>
