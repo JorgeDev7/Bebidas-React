19fb02af4908fadf948353b84f25a17c54971296
JorgeDev <jorgehumbertocolindres@gmail.com>
2024-05-13 11:09:37 -0600

    adding the notification if the favorite was added or not

--- src/stores/favoritesSlice.ts
+++ src/stores/favoritesSlice.ts
@@ -1,6 +1,7 @@
 import { StateCreator } from 'zustand';
 import type { Recipe } from '../types';
 import { RecipesSlideType, createRecipesSlice } from './recipeSlide';
+import { NotificationSliceType, createNotificationSlice } from './notificationSlice';
 
 export type FavoritesSliceType = {
     favorites: Recipe[],
@@ -10,17 +11,25 @@ export type FavoritesSliceType = {
     removeFavorites: () => void;
 };
 
-export const createFavoritesSlice: StateCreator<FavoritesSliceType & RecipesSlideType, [], [], FavoritesSliceType> = (set, get, api) => ({
+export const createFavoritesSlice: StateCreator<FavoritesSliceType & RecipesSlideType & NotificationSliceType, [], [], FavoritesSliceType> = (set, get, api) => ({
     favorites: [],
     handleClickFavorite: (recipe) => {
         if (get().favoriteExists(recipe.idDrink)) {
             set((state) => ({
                 favorites: state.favorites.filter(favorite => favorite.idDrink !== recipe.idDrink)
             }));
+            createNotificationSlice(set, get, api).showNotification({
+                text: 'Se eliminó de Favoritos',
+                error: false
+            });
         } else {
             set((state) => ({
                 favorites: [...state.favorites, recipe]
             }));
+            createNotificationSlice(set, get, api).showNotification({
+                text: 'Se agregó a Favoritos',
+                error: false
+            });
         }
 
         createRecipesSlice(set, get, api).closeModal();
